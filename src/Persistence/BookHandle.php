a08f324611a699c8fd3b18d7d38ca134720c5f01
GH0STYtopflo <soapss85@proton.me>
2026-08-13 19:48:58 +0330

    feat: Add a persistence class for interacting with the books table

---
+++ src/Persistence/BookHandle.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Gh0stytopflo\PhpLib\Persistence;
+
+use Gh0stytopflo\PhpLib\Model\Book;
+
+class BookHandle
+{
+    private const PATH_TO_FILE = __DIR__ . '/../../tables/book.csv';
+
+    public static function append(Book $book)
+    {
+        $file = fopen(self::PATH_TO_FILE, 'a+');
+
+        fputcsv($file, $book->getPropertyArray(), ',');
+    }
+
+    public static function readAll(): array
+    {
+        $file = fopen(self::PATH_TO_FILE, 'r');
+        $records = [];
+
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0, ',');
+
+            if (!is_bool($record)) {
+                $records[] = $record;
+            }
+        }
+
+        return $records;
+    }
+
+    public static function writeAll(array $records): void
+    {
+        $file = fopen(self::PATH_TO_FILE, 'w+');
+
+        foreach ($records as $record) {
+            fputcsv($file, $record, ',');
+        }
+    }
+
+    public static function search(
+        int $id = null,
+        string $title = null,
+        string $author = null,
+        int $year = null,
+        int $printing = null,
+        string $genre = null
+    ): array {
+        $file = fopen(self::PATH_TO_FILE, 'r');
+        $records = [];
+
+        while (!feof($file)) {
+            $record = fgetcsv($file, 0, ',');
+
+            if (!is_bool($record)) {
+                if (
+                    (!isset($id) || $id == $record[0])
+                    && (!isset($title) || str_contains(strtolower($record[1]), strtolower($title)))
+                    && (!isset($author) || str_contains(strtolower($record[2]), strtolower($author)))
+                    && (!isset($year) || $year == $record[3])
+                    && (!isset($printing) || $year == $record[4])
+                    && (!isset($genre) || str_contains(strtolower($record[5]), strtolower($genre)))
+                ) {
+                    $records[] = $record;
+                }
+            }
+        }
+
+        return $records;
+    }
+}
