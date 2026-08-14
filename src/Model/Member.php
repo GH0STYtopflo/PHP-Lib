5dc345f767b45b4ff1aef43d2034a369bdfbd833
GH0STYtopflo <soapss85@proton.me>
2026-08-14 09:24:48 +0330

    feat: Add member and person classes

---
+++ src/Model/Member.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Gh0stytopflo\PhpLib\Model;
+
+use Gh0stytopflo\PhpLib\Model\Person;
+
+class Member extends Person
+{
+    private int $membershipStartDate;
+    private int $memberId;
+
+    public function __construct(
+        string $name,
+        string $lname,
+        int $membershipStartDate,
+        ?int $memberId = null
+    ) {
+        //TODO: IdGen
+        if (isset($memberId)) {
+            $this->memberId = $memberId;
+        }
+        $this->name = $name;
+        $this->lname = $lname;
+        $this->membershipStartDate = $membershipStartDate;
+    }
+
+    public static function mapArrayToInstance(array $csvRecord)
+    {
+        return new self(
+            memberId: $csvRecord[0],
+            name: $csvRecord[1],
+            lname: $csvRecord[2],
+            membershipStartDate: $csvRecord[3]
+        );
+    }
+
+    public function getPropertyArray(): array
+    {
+        return array(
+            $this->memberId,
+            $this->name,
+            $this->lname,
+            $this->membershipStartDate
+        );
+    }
+
+    public function printProperties(): void
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lname;
+    }
+
+    public function getMembershipStartDate(): int
+    {
+        return $this->membershipStartDate;
+    }
+
+    public function getMemberId(): int
+    {
+        return $this->memberId;
+    }
+}
---
+++ src/Model/Person.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Gh0stytopflo\PhpLib\Model;
+
+abstract class Person
+{
+    protected string $name;
+    protected string $lname;
+
+    abstract public function printProperties(): void;
+}
