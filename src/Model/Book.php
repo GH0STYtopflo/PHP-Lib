b4f564d9bf7f54c8891569f0ce5938108db98394
GH0STYtopflo <soapss85@proton.me>
2026-08-14 08:58:51 +0330

    feat: Add a csv array to book instance method

--- src/Model/Book.php
+++ src/Model/Book.php
@@ -10,18 +10,49 @@ class Book
     private int $year;
     private int $printing;
     private string $genre;
-    private int $memberId;
-    private int $borrowDate;
-    private int $returnDate;
+    private ?int $memberId;
+    private ?int $borrowDate;
+    private ?int $returnDate;
 
-    public function __construct(string $title, string $author, int $year, int $printing, string $genre)
-    {
-        //TODO: IdGen
+    public function __construct(
+        string $title,
+        string $author,
+        int $year,
+        int $printing,
+        string $genre,
+        ?int $bookId = null,
+        ?int $memberID = null,
+        ?int $borrowDate = null,
+        ?int $returnDate = null
+    ) {
+        if (isset($bookId)) {
+            $this->bookId = $bookId;
+        }
+
         $this->author = $author;
         $this->title = $title;
         $this->year = $year;
         $this->printing = $printing;
         $this->genre = $genre;
+        $this->memberId = $memberID;
+        $this->borrowDate = $borrowDate;
+        $this->returnDate = $returnDate;
+    }
+
+    public static function mapArrayToInstance(array $csvRecord): self
+    {
+        // TODO: Throw an exception for invalid input
+        return new self(
+            bookId: (int) $csvRecord[0],
+            title: $csvRecord[1],
+            author: $csvRecord[2],
+            year: (int) $csvRecord[3],
+            printing: (int) $csvRecord[4],
+            genre: $csvRecord[5],
+            memberID: !empty($csvRecord[6]) ? (int) $csvRecord[6] : null,
+            borrowDate: !empty($csvRecord[7]) ? (int) $csvRecord[7] : null,
+            returnDate: !empty($csvRecord[8]) ? (int) $csvRecord[8] : null,
+        );
     }
 
     public function getPropertyArray(): array
